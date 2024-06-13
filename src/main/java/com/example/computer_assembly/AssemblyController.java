a4cd78c0208c967b8caa3280dba96d8c74eaef99
Werterwy <orazbaijoldasbaevich@gmail.com>
2024-06-13 15:48:12 +0500

    working fxml

--- src/main/java/com/example/computer_assembly/AssemblyController.java
+++ src/main/java/com/example/computer_assembly/AssemblyController.java
@@ -48,53 +48,53 @@ public class AssemblyController {
     private Timeline timeline;
     private int timeSeconds = 300; // 5 минут = 300 секунд
 
-    @FXML
-    public void initialize() {
-        // Load the instruction image
-        Image instructionImage = new Image(getClass().getResource("/motherboard.png").toExternalForm());
-        if (instructionImage != null) {
-            assemblyImageView.setImage(instructionImage);
-        } else {
-            System.err.println("Ошибка: Не удалось загрузить изображение instruction.png");
-        }
-
-        Image clipartimage = new Image(getClass().getResource("/clipart.png").toExternalForm());
-        assemblyClipartImageView.setImage(clipartimage);
-
-        Image cpuImage = new Image(getClass().getResource("/cpu.png").toExternalForm());
-        assemblyCPUImageView.setImage(cpuImage);
-
-        Image ramImage = new Image(getClass().getResource("/ram.png").toExternalForm());
-        assemblyRAMImageView.setImage(ramImage);
-
-        Image blokImage = new Image(getClass().getResource("/blok-pitaniya1.png").toExternalForm());
-        assemblyBlokImageView.setImage(blokImage);
-
-        Image hardDiskImage = new Image(getClass().getResource("/Hard-Disk.png").toExternalForm());
-        assemblyHardDiskImageView.setImage(hardDiskImage);
-
-        Image pointImage = new Image(getClass().getResource("/point1.png").toExternalForm());
-        assemblyPoint1ImageView.setImage(pointImage);
-        assemblyPoint2ImageView.setImage(pointImage);
-        assemblyPoint3ImageView.setImage(pointImage);
-        assemblyPoint4ImageView.setImage(pointImage);
-        assemblyPoint5ImageView.setImage(pointImage);
-
-        // Инициализируем таймер
-        timerLabel.setText(formatTime(timeSeconds));
-        timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
-            timeSeconds--;
-            timerLabel.setText(formatTime(timeSeconds));
-            if (timeSeconds <= 0) {
-                timeline.stop();
-                showResults();
-            }
-        }));
-        timeline.setCycleCount(Timeline.INDEFINITE);
-        timeline.play();
-
-
-    }
+//    @FXML
+//    public void initialize() {
+//        // Load the instruction image
+//        Image instructionImage = new Image(getClass().getResource("/motherboard.png").toExternalForm());
+//        if (instructionImage != null) {
+//            assemblyImageView.setImage(instructionImage);
+//        } else {
+//            System.err.println("Ошибка: Не удалось загрузить изображение instruction.png");
+//        }
+//
+//        Image clipartimage = new Image(getClass().getResource("/clipart.png").toExternalForm());
+//        assemblyClipartImageView.setImage(clipartimage);
+//
+//        Image cpuImage = new Image(getClass().getResource("/cpu.png").toExternalForm());
+//        assemblyCPUImageView.setImage(cpuImage);
+//
+//        Image ramImage = new Image(getClass().getResource("/ram.png").toExternalForm());
+//        assemblyRAMImageView.setImage(ramImage);
+//
+//        Image blokImage = new Image(getClass().getResource("/blok-pitaniya1.png").toExternalForm());
+//        assemblyBlokImageView.setImage(blokImage);
+//
+//        Image hardDiskImage = new Image(getClass().getResource("/Hard-Disk.png").toExternalForm());
+//        assemblyHardDiskImageView.setImage(hardDiskImage);
+//
+//        Image pointImage = new Image(getClass().getResource("/point1.png").toExternalForm());
+//        assemblyPoint1ImageView.setImage(pointImage);
+//        assemblyPoint2ImageView.setImage(pointImage);
+//        assemblyPoint3ImageView.setImage(pointImage);
+//        assemblyPoint4ImageView.setImage(pointImage);
+//        assemblyPoint5ImageView.setImage(pointImage);
+//
+//        // Инициализируем таймер
+//        timerLabel.setText(formatTime(timeSeconds));
+//        timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
+//            timeSeconds--;
+//            timerLabel.setText(formatTime(timeSeconds));
+//            if (timeSeconds <= 0) {
+//                timeline.stop();
+//                showResults();
+//            }
+//        }));
+//        timeline.setCycleCount(Timeline.INDEFINITE);
+//        timeline.play();
+//
+//
+//    }
 
    @FXML
    protected void onBackMainClick() {
