a6689039d9dfe42c985abd4619c71a5bef9e5e2b
Werterwy <orazbaijoldasbaevich@gmail.com>
2024-06-13 11:40:02 +0500

    test

--- src/main/java/com/example/computer_assembly/HelloApplication.java
+++ src/main/java/com/example/computer_assembly/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 801);
         stage.setTitle("Computer Assembly Trainer");
         stage.setScene(scene);
         stage.show();
